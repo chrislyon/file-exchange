38987ca6eacfb0dde14f6b85776367106241ff87
chris lyon tech <chris.lyon.tech@gmail.com>
2012-12-16 18:36:15 +0100

    Qq modifs

--- filex/filex/views.py
+++ filex/filex/views.py
@@ -1,11 +1,20 @@
 from django.template import Context, loader
 from django.http import HttpResponse
 
+import datetime
+
 def hello(request):
     return HttpResponse("Hello, world.")
 
 
 def index(request):
     t = loader.get_template('tmpl/index.html')
-    c = Context({ 'DATA': "TEST DE DATA", })
+    d = datetime.datetime.now()
+    PUB_DATE = d.strftime("%d/%m/%Y %X")
+
+    c = Context({ 
+        'DATA': "TEST DE DATA", 
+        'PUB_DATE':PUB_DATE, 
+        'TITRE_PAGE' : 'WELCOME',
+        })
     return HttpResponse(t.render(c))
