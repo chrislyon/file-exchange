7d786ba50501c28dd2b22c069564ecb7a681b000
chris lyon tech <chris.lyon.tech@gmail.com>
2012-12-22 12:21:43 +0100

    Ajout d'une feuille de style pour avoir une joli form et cela focntionne !! merci a pompage.net

--- filex/fex/models.py
+++ filex/fex/models.py
@@ -22,7 +22,7 @@ class EntiteClass(models.Model):
         return "%s : %s" % (self.codent, self.noment)
 
 class EntiteForm(forms.Form):
-    codent = forms.CharField(label='Code Entité : ',max_length=20, required=True)
-    noment = forms.CharField(label='Nom Entité : ',max_length=40, required=False)
-    description = forms.MultiValueField(label='Description : ',required=False, widget=forms.Textarea)
-    typent = forms.ChoiceField( label='Type Entité : ',choices=TYPE_ENT, required=False )
+    codent = forms.CharField(label='Code Entité ',max_length=20, required=True)
+    noment = forms.CharField(label='Nom Entité ',max_length=40, required=False)
+    description = forms.MultiValueField(label='Description ',required=False, widget=forms.Textarea)
+    typent = forms.ChoiceField( label='Type Entité ',choices=TYPE_ENT, required=False )
